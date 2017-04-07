6296dd0c471147ba26c016b2d12647e5c9bd3efa
client <client@epam.com>
2017-04-07 20:52:29 +0300

    Task7

---
+++ cookbooks/jboss/metadata.rb
@@ -0,0 +1,8 @@
+name             'jboss'
+maintainer       'Anton Kaminski'
+maintainer_email 'anton_kaminski@epam.com'
+license          'All rights reserved'
+description      'Installs/Configures JBOSS'
+version          '0.1.0'
+
+depends          'java'
---
+++ cookbooks/jboss/recipes/default.rb
@@ -0,0 +1,3 @@
+include_recipe 'java'
+include_recipe 'jboss::install'
+include_recipe 'jboss::deploy'
